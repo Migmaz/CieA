ddd7f6a2c5bcffbc6874b55d716a6b304852822a
Migmaz <140793488+Migmaz@users.noreply.github.com>
2026-04-10 09:03:50 -0400

    update

--- FollowGap/motor.py
+++ FollowGap/motor.py
@@ -34,3 +34,29 @@ def send_command(cmd_vel):
         Convertit en PWM et contrôle les moteurs.
     """
     pass
+
+
+import time
+from pynput import keyboard
+
+
+
+
+time_int = time.time()
+key_temp = None
+while True :
+    with keyboard.Events() as events:
+    # Block at most one second
+        event = events.get(0.1)
+        if event is None:
+            print(key_temp)
+        else:
+            print('Received event {}'.format(event))
+            print(event.key)
+            key_temp = event.key
+            if event.key == keyboard.Key.esc :
+                break
+    print(time.time()-time_int)
+    print("test")
+    
+    
--- main.py
+++ main.py
@@ -15,7 +15,7 @@ Rôle :
 # =================================================
 import time
 import numpy as np
-
+from pynput import keyboard
 
 # =================================================
 # Importation de librairy Périphérique
@@ -42,10 +42,7 @@ from FollowGap import FTG, FSM, tool
 # =================================================
 
 # IMU
-i2c_IMU = busio.I2C(board.SCL, board.SDA, frequency=100000)
-bno = BNO08X_I2C(i2c_IMU)
-bno.enable_feature(BNO_REPORT_ACCELEROMETER)
-bno.enable_feature(BNO_REPORT_ROTATION_VECTOR)
+
 
 # PWM
 i2c_PWM = board.I2C()
@@ -53,6 +50,62 @@ pca = PCA9685(i2c_PWM)
 pca.frequency = 60
 
 # =================================================
-# Boucle principale
+# Sélection mode de contrôle
 # =================================================
 
+print("Veuillez choisir le mode de contrôle pour le rover : ")
+print(" 1 - Mode autonome : Follow the gap")
+print(" 2 - Mode manuel : Clavier AWSD")
+choix_mode = input("Entrer le choix du mode : ")
+
+if choix_mode == 1 :
+    print("Contrôle autonome sélectionner")
+    # =================================================
+    # Boucle contrôle autonome
+    # =================================================
+    
+    # Initialisation du IMU
+    i2c_IMU = busio.I2C(board.SCL, board.SDA, frequency=100000)
+    bno = BNO08X_I2C(i2c_IMU)
+    bno.enable_feature(BNO_REPORT_ACCELEROMETER)
+    bno.enable_feature(BNO_REPORT_ROTATION_VECTOR)
+    
+    
+    
+else : 
+    print("Contrôle manuel sélectionner")
+    # =================================================
+    # Boucle contrôle manuel
+    # =================================================
+    
+    # Initialisation de la lecture du clavier
+    def on_press(key, injected):
+        try:
+            None
+        except AttributeError:
+            print('special key {} pressed'.format(
+                key))
+
+    def on_release(key, injected):
+        if key == keyboard.Key.esc:
+            # Stop listener
+            return False
+
+    # Collect events until released
+    listener = keyboard.Listener()
+    listener.start()
+    
+    # Boucle principale
+    while True:
+        with keyboard.Events() as events:
+        # Block at most one second (à modifier pour fit la boucle)
+            event = events.get(1.0)
+            if event is None:
+                print('You did not press a key within one second')
+            else:
+                print(event.key)
+                
+                # Garde en mémoire la dernier touche 
+                key_temp = event.key
+                # Ferme le programme avec la touche escape
+                if event.key == keyboard.Key.esc : break
